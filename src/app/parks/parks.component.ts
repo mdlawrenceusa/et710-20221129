27d3c3c1ccd4596dab1234a60d0d1cb0fd25318f
mdlawrenceusa <mdlawrence@me.com>
2022-11-29 11:58:07 -0500

    1 file modified

--- src/app/parks/parks.component.ts
+++ src/app/parks/parks.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { products } from '../products';
+
 @Component({
   selector: 'app-parks',
   templateUrl: './parks.component.html',
-  styleUrls: ['./parks.component.css']
+  styleUrls: ['./parks.component.css'],
 })
 export class ParksComponent {
-
+  products = products;
 }
